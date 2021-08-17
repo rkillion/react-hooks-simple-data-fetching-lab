248b84c9f0f5e48e09e55b71accbd1feaef2e16e
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-17 18:04:42 -0400

    commit tree 31d1908ce6c142b61dd90665849ea67bfd4bcdf2

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
+import { useEffect, useState } from "react"
 // create your App component here
+export default function App(){
+    const [imageURL,setImageURL] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random").then(r=>r.json()).then(data=>{setImageURL(data.message)});
+    }, [])
+
+    if (!imageURL) {
+        return <p>Loading...</p>
+    } else {
+        return <img 
+        src={imageURL}
+        alt="A Random Dog"
+        />
+    }
+}
